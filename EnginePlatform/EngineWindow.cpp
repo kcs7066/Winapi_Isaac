b1d16ccf3d8d48ffe7e446c092c400399fb28f64
kcs7066 <kcs7066@naver.com>
2024-10-23 12:28:15 +0900

    김창수 : 업데이트

--- EnginePlatform/EngineWindow.cpp
+++ EnginePlatform/EngineWindow.cpp
@@ -1,11 +1,8 @@
 #include "EngineWindow.h"
+#include <EngineBase/EngineDebug.h>
 
 HINSTANCE UEngineWindow::hInstance = nullptr;
-
-void UEngineWindow::EngineWindowInit(HINSTANCE _Instance)
-{
-	hInstance = _Instance;
-}
+std::map<std::string, WNDCLASSEXA> UEngineWindow::WindowClasses;
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
@@ -27,6 +24,28 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     return 0;
 }
 
+void UEngineWindow::EngineWindowInit(HINSTANCE _Instance)
+{
+    WNDCLASSEXA wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = nullptr;
+    wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName = nullptr;
+    wcex.lpszClassName = "Default";
+    wcex.hIconSm = nullptr;
+    CreateWindowClass(wcex);
+    
+    hInstance = _Instance;
+}
+
 int UEngineWindow::WindowMessageLoop()
 {
 	MSG msg;
@@ -44,33 +63,25 @@ int UEngineWindow::WindowMessageLoop()
 
 }
 
-UEngineWindow::UEngineWindow()
+void UEngineWindow::CreateWindowClass(const WNDCLASSEXA& _Class)
 {
-    WNDCLASSEXA wcex;
+    std::map<std::string, WNDCLASSEXA>::iterator EndIter = WindowClasses.end();
+    std::map<std::string, WNDCLASSEXA>::iterator FindIter = WindowClasses.find(std::string(_Class.lpszClassName));
 
-    wcex.cbSize = sizeof(WNDCLASSEX);
-
-    wcex.style = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc = WndProc;
-    wcex.cbClsExtra = 0;
-    wcex.cbWndExtra = 0;
-    wcex.hInstance = hInstance;
-    wcex.hIcon = nullptr;
-    wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-    wcex.lpszMenuName = nullptr;
-    wcex.lpszClassName = "DefaultWindow";
-    wcex.hIconSm = nullptr;
-
-    RegisterClassExA(&wcex);
-
-    WindowHandle = CreateWindowA("DefaultWindow", "MainWindow", WS_OVERLAPPEDWINDOW,
-        0, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
-
-    if (!WindowHandle)
+    if (EndIter != FindIter)
     {
+        MSGASSERT(std::string(_Class.lpszClassName) + " 같은 이름의 윈도우 클래스를 2번 등록했습니다");
         return;
     }
+
+    RegisterClassExA(&_Class);
+
+    WindowClasses.insert(std::pair{ _Class.lpszClassName, _Class });
+}
+
+UEngineWindow::UEngineWindow()
+{
+  
 }
 
 UEngineWindow::~UEngineWindow()
@@ -78,8 +89,36 @@ UEngineWindow::~UEngineWindow()
 
 }
 
-void UEngineWindow::Open()
+void UEngineWindow::Create(std::string_view _ClassName )
 {
+    Create("Window", _ClassName);
+}
+
+void UEngineWindow::Create(std::string_view _TitleName, std::string_view _ClassName)
+{
+    if (false == WindowClasses.contains(_ClassName.data()))
+    {
+        MSGASSERT(std::string(_ClassName) + " 등록하지 않은 클래스로 윈도우창을 만들려고 했습니다");
+        return;
+    }
+
+    WindowHandle = CreateWindowA(_ClassName.data(), _TitleName.data(), WS_OVERLAPPEDWINDOW,
+        0, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+    if (!WindowHandle)
+    {
+        MSGASSERT("윈도우 생성에 실패했습니다." + std::string(_TitleName));
+        return;
+    }
+
+}
+
+void UEngineWindow::Open(std::string_view _TitleName)
+{
+    if (nullptr == WindowHandle)
+    {
+        Create();
+    }
 
 	ShowWindow(WindowHandle, SW_SHOW);
 	UpdateWindow(WindowHandle);
--- EnginePlatform/EngineWindow.h
+++ EnginePlatform/EngineWindow.h
@@ -1,10 +1,14 @@
 #pragma once
 #include <Windows.h>
+#include <string>
+#include <map>
 
 class UEngineWindow
 {
 public:
 	static void EngineWindowInit(HINSTANCE _Instance);
+	static void CreateWindowClass(const WNDCLASSEXA& _Class);
+	static bool IsWindowClass(const std::string_view _Text);
 
 	static int WindowMessageLoop();
 
@@ -16,13 +20,16 @@ public:
 	UEngineWindow& operator=(const UEngineWindow& _Other) = delete;
 	UEngineWindow& operator=(UEngineWindow&& _Other) noexcept = delete;
 
-	void Open();
+	void Create(std::string_view _TitleName, std::string_view _ClassName = "Default");
+	void Create(std::string_view _ClassName = "Default");
+	void Open(std::string_view _TitleName = "Window");
 
 protected:
 
 private:
 
 	static HINSTANCE hInstance;
+	static std::map<std::string, WNDCLASSEXA> WindowClasses;
 
 	HWND WindowHandle = nullptr;
 };
